448fa36a3ad529582e20b788ce7d71b81271b0c1
차승엽 <chaseung-yeob@chaseung-yeob-ui-MacBookAir.local>
2023-12-29 22:05:45 +0900

    12/29

--- silver/16139.py
+++ silver/16139.py
@@ -9,5 +9,7 @@ for i in range(n):
     arr = input().split()
     score = 0
     for j in range(int(arr[1]),int(arr[2]) + 1):
-        if str[j] == arr[0]:
+        if name[j] == arr[0]:
+
             score += 1
+    print(score)
